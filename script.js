8db9ded02c88acf02b800d51f482f135a80c3520
Laura-html <lrichards023@gmail.com>
2025-04-13 13:19:16 +0100

    Added API integration in JS

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 let apiKey = "9245bb24b027319eb10oat71638b73cf";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
-let prompt = "";
-let context = "";
-let instructionsInput = document.querySelector("#instructions");
+let instructionsInput = document.querySelector("#dish");
+let context =
+  "You are a world-renowned chef in gluten-free dishes. Your mission is to list a recipe in basic HTML. Make sure to follow user instructions.At the end, sign the recipe with SheCodes AI.";
+let prompt = `user instructions: generate a gluten-free ${instructionsInput} recipe`;
 
-function displayRecipe(response) {
-  recipeElement.innerHTML = "response.data.answer";
-}
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 function generateRecipe(event) {
   event.preventDefault();
-  let recipeElement = document.querySelector("#recipe");
 }
+
+let recipeElement = document.querySelector("#recipe");
+
+function displayRecipe(response) {
+  recipeElement.innerHTML = response.data.answer;
+}
+
 let recipeFormElement = document.querySelector("#recipe-generator");
 recipeFormElement.addEventListener("submit", generateRecipe);
 
