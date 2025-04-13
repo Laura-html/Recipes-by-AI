ad749b6a300552ba8c3d08da29aea405f669d48f
Laura-html <lrichards023@gmail.com>
2025-04-13 09:10:52 +0100

    added basic javascript

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("hello");
+function generateRecipe(event) {
+  event.preventDefault();
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.innerHTML = "Enjoy!";
+}
+let recipeFormElement = document.querySelector("#recipe-generator");
+recipeFormElement.addEventListener("submit", generateRecipe);
