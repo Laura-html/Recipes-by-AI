8de094e9e61b01b20b26407546d3e235307f7686
Laura-html <lrichards023@gmail.com>
2025-04-13 11:53:21 +0100

    started adding javascript to generate recipe

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+let apiKey = "9245bb24b027319eb10oat71638b73cf";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+let prompt = "";
+let context = "";
+let instructionsInput = document.querySelector("#instructions");
+
+function displayRecipe(response) {
+  recipeElement.innerHTML = "response.data.answer";
+}
+
 function generateRecipe(event) {
   event.preventDefault();
   let recipeElement = document.querySelector("#recipe");
-  recipeElement.innerHTML = "Enjoy!";
 }
 let recipeFormElement = document.querySelector("#recipe-generator");
 recipeFormElement.addEventListener("submit", generateRecipe);
+
+axios.get(apiUrl).then(displayRecipe);
